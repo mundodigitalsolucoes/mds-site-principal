f59f2837605ee4ad61d4e9e0f994edbe97c4822a
Fábio Alves <fabio@mundodigitalsolucoes.com.br>
2026-08-29 23:15:00 -0300

    feat(blog): registra Bora Vender+ setembro 2026

--- src/content/blog/index.ts
+++ src/content/blog/index.ts
@@ -1,8 +1,10 @@
 import metodoMds from "./o-que-e-o-metodo-mds";
 import marketingVendasPmes from "./marketing-e-vendas-pmes";
 import siteComumVsSiteInteligente from "./site-comum-vs-site-inteligente";
+import boraVenderSetembro2026 from "./bora-vender-setembro-2026";
 
 export const blogPosts = [
+  boraVenderSetembro2026,
   metodoMds,
   marketingVendasPmes,
   siteComumVsSiteInteligente,
