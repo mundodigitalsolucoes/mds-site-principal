835322a81fb6fbb1ec52801588229a734f96f6e2
Fábio Alves <fabio@mundodigitalsolucoes.com.br>
2026-09-04 22:55:33 -0300

    Add global Google Tag Manager tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import AtendimentoWidget from "@/components/layout/AtendimentoWidget";
+
+const GTM_ID = "GTM-P2GZ7PL9";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://mundodigitalsolucoes.com.br"),
@@ -32,6 +35,21 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body>
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+        </Script>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Header />
         <main>{children}</main>
         <Footer />
