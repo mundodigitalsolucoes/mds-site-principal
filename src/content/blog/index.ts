5b04672c1545244db5b7dcb13bc1dbeac3dba9f1
Fábio Alves <fabio@mundodigitalsolucoes.com.br>
2026-09-08 10:26:04 -0300

    feat: register Dia do Cliente article

--- src/content/blog/index.ts
+++ src/content/blog/index.ts
@@ -3,8 +3,10 @@ import marketingVendasPmes from "./marketing-e-vendas-pmes";
 import siteComumVsSiteInteligente from "./site-comum-vs-site-inteligente";
 import boraVenderSetembro2026 from "./bora-vender-setembro-2026";
 import campanhasPizzaria from "./12-campanhas-para-pizzaria-aumentar-faturamento";
+import diaDoCliente from "./dia-do-cliente-como-vender-mais";
 
 export const blogPosts = [
+  diaDoCliente,
   campanhasPizzaria,
   boraVenderSetembro2026,
   metodoMds,
